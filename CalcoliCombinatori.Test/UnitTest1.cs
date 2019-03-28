6779a495b084b15f4ae0860df86d9153fad2e003
ITT Rosselli Rasetti <rosselli.rasetti@outlook.com>
2019-03-28 08:44:48 +0100

    Add project files.

---
+++ CalcoliCombinatori.Test/UnitTest1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CalcoliCombinatori.Test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [DataTestMethod]
+        [DataRow(0,1)]
+        [DataRow(1,1)]
+        [DataRow(5, 120)]
+        [DataRow(10, 3628800)]
+        [DataRow(50, 0)]
+        public void TestMethod1(long numero,long risultato)
+        {
+            long fattoriale;
+
+            fattoriale = EquazioniLibrary.CalcoliCombinatori.Fattoriale(numero);
+            Assert.AreEqual(fattoriale, risultato);
+        }
+    }
+}
---
+++ EquazioniLibrary/CalcoliCombinatori.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EquazioniLibrary
+{
+    public class CalcoliCombinatori
+    {
+     public static long Fattoriale(long numero)
+        {
+            long risultato = 1;
+
+            if(numero<20)
+            {
+                for (int i = 1; i <= numero; i++)
+                {
+                    risultato = risultato * i;
+                }
+            }
+            else
+            {
+                risultato = 0;
+            }
+            return risultato;
+
+        }
+    }
+}
+
